19bb29c2fcf114993cacc6450eb9355911a875d8
Aabishkar Shrestha <Aabishkar@adsaro.com>
2025-03-24 11:28:41 +0545

    Updated video section , Highlighted Features

--- resources/views/frontend/home/sauryapellet_video.blade.php
+++ resources/views/frontend/home/sauryapellet_video.blade.php
@@ -1,39 +1,4 @@
-{{-- <section class="py-8 mx-auto overflow-hidden bg-white light text-stone-800 max-w-screen-2xl">
-    <div class="px-10 max-sm:px-4">
-        <div class="grid justify-center grid-cols-1">
-            <div class="col-span-1">
-                <div class="">
-                    <div class="flex flex-wrap items-center justify-between ">
-                        <h2 class="max-sm:text-lg text-2xl font-bold text-left text-[#213343] md:text-2xl ">
-                            SSurya Krishi Video
-                        </h2>
-                        <a href="{{ route('videos') }}"
-                            class="px-4 max-sm:px-2  py-2 flex text-[#213343] items-center hover:underline ">
-                            View All
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-right"
-                                width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                <path d="M5 12l14 0"></path>
-                                <path d="M13 18l6 -6"></path>
-                                <path d="M13 6l6 6"></path>
-                            </svg>
-                        </a>
-                    </div>
-                    <div class="flex py-3">
-                        <div class="w-[10%] border border-[#f15a28]"></div>
-                        <div class="w-[90%] border"></div>
-                    </div>
 
-                </div>
-            </div>
-
-            <div class="relative ">
-                @include('frontend.home.video')
-            </div>
-        </div>
-    </div>
-</section> --}}
 
 
 
@@ -44,9 +9,9 @@
 
     <div class="flex flex-col md:flex-row">
         <!-- can help image -->
-        <div class="mr-0 md:mr-8 mt-1  md:mb-0 max-sm:mb-3">
+        {{-- <div class="mr-0 md:mr-8 mt-1  md:mb-0 max-sm:mb-3">
             <img class="w-[300px] h-[440px] mx-auto" src="{{ asset('images/World largest dam.jpg') }}" alt="can_help_banner">
-          </div>
+          </div> --}}
 
         <!-- end can help image -->
 
